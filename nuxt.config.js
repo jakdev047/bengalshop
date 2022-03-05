81086e9680a1771522d6ef1f05bdb719b639136d
jakdev047 <jakfreelancer047@gmail.com>
2022-03-05 20:37:46 +0600

    vue slick carousel plugin use

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    
+    {src: '~/plugins/vueSlickCarousel', ssr: true},
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
