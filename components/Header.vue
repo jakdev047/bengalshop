88a0adf4315dd2f96ddb9362a8ac89e2b6b9580f
jakdev047 <jakfreelancer047@gmail.com>
2022-03-05 15:21:54 +0600

    layout create

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+        <nuxt-link to="/about">About</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>This is home page </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+  }
+</script>
